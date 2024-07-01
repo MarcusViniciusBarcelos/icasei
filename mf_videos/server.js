be3345efac7fe23e10a761d9989ec2ab0542508f
Marcus Barcelos <marcusbarcelos2001@gmail.com>
2024-07-01 15:32:14 -0300

    .

--- mf_videos/server.js
+++ mf_videos/server.js
@@ -1,9 +1,32 @@
 const express = require('express');
+const path = require('path');
+const axios = require('axios');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 const app = express();
 const port = 3001;
 
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/css', express.static(path.join(__dirname, 'src/css')));
+app.use('/js', express.static(path.join(__dirname, 'src/js')));
+
 app.get('/', (req, res) => {
-  res.send('Hello from mf_videos!');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/search', async (req, res) => {
+  const query = req.query.q;
+  const apiKey = process.env.YOUTUBE_API_KEY;
+  const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&q=${query}&key=${apiKey}`;
+
+  try {
+    const response = await axios.get(url);
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 });
 
 app.listen(port, () => {
